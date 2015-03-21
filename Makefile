ffd19d23b57b75929fcf62afa4a4227660cc1fa9
Vlado Boza <bozavlado@gmail.com>
2015-03-21 11:19:24 +0100

    cleanup

--- Makefile
+++ Makefile
@@ -1,38 +1,8 @@
 CFLAGS = -O3 -Wall -Wextra -fno-strict-aliasing -g
 
-ALL = daligner HPCdaligner HPCmapper LAsort LAmerge LAsplit LAcat LAshow LAcheck
+ALL = test_align
 
 all: $(ALL)
-
-daligner: daligner.c filter.c filter.h align.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o daligner daligner.c filter.c align.c DB.c QV.c -lpthread -lm
-
-HPCdaligner: HPCdaligner.c DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o HPCdaligner HPCdaligner.c DB.c QV.c -lm
-
-HPCmapper: HPCmapper.c DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o HPCmapper HPCmapper.c DB.c QV.c -lm
-
-LAsort: LAsort.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAsort LAsort.c DB.c QV.c -lm
-
-LAmerge: LAmerge.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAmerge LAmerge.c DB.c QV.c -lm
-
-LAshow: LAshow.c align.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAshow LAshow.c align.c DB.c QV.c -lm
-
-LAcat: LAcat.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAcat LAcat.c DB.c QV.c -lm
-
-LAsplit: LAsplit.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAsplit LAsplit.c DB.c QV.c -lm
-
-LAcheck: LAcheck.c align.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAcheck LAcheck.c align.c DB.c QV.c -lm
-
-LAupgrade.Dec.31.2014: LAupgrade.Dec.31.2014.c align.c align.h DB.c DB.h QV.c QV.h
-	gcc $(CFLAGS) -o LAupgrade.Dec.31.2014 LAupgrade.Dec.31.2014.c align.c DB.c QV.c -lm
 
 QV.o: QV.c QV.h
 	gcc $(CFLAGS) -c QV.c
@@ -54,7 +24,7 @@ clean:
 	rm -fr *.dSYM
 	rm -f LAupgrade.Dec.31.2014
 	rm -f daligner.tar.gz
-	rm test_align *.o
+	rm -f *.o
 
 install:
 	cp $(ALL) ~/bin
--- test_align.cc
+++ test_align.cc
@@ -7,24 +7,26 @@ extern "C" {
 
 char *TranslateSeq(char* x) {
   int l = strlen(x);
-  char *ret = new char[l+1];
+  char *ret = new char[l+2];
+  ret[0] = 4;
   for (int i = 0; i < l; i++) {
-    if (x[i] == 'A') ret[i] = 0;
-    if (x[i] == 'C') ret[i] = 1;
-    if (x[i] == 'G') ret[i] = 2;
-    if (x[i] == 'T') ret[i] = 3;
+    if (x[i] == 'A') ret[i+1] = 0;
+    if (x[i] == 'C') ret[i+1] = 1;
+    if (x[i] == 'G') ret[i+1] = 2;
+    if (x[i] == 'T') ret[i+1] = 3;
   }
-  ret[l] = 4;
-  return ret;
+  ret[l+1] = 4;
+  return ret+1;
 }
 
 int main() {
   Work_Data *work_data = New_Work_Data();
   float freq[] = {0.25, 0.25, 0.25, 0.25};
-  Align_Spec *align_spec = New_Align_Spec(0.7, 1, freq);
+  // Tuna sa treba pozriet na parametre
+  Align_Spec *align_spec = New_Align_Spec(0.7, 2, freq);
 
-  char seq1[] = "AAACCCTTTGGGAAA";
-  char seq2[] = "AAACCCTTTGGGAAA";
+  char seq1[] = "AAACCCTTTGGGAAAC";
+  char seq2[] = "AAACCCTGTGGGAAAC";
 
   Alignment al;
   Path p;
@@ -32,18 +34,18 @@ int main() {
   al.flags = 0;
   al.aseq = TranslateSeq(seq1);
   al.bseq = TranslateSeq(seq2);
-  al.alen = 15;
-  al.blen = 15;
+  al.alen = 16;
+  al.blen = 16;
 
   printf("go al\n");
-  Local_Alignment(&al, work_data, align_spec, 1, 1, 1); 
+  Local_Alignment(&al, work_data, align_spec, 2, 2, 4); 
   
   FILE *ca = fopen("cartoon.dat", "w");
   printf("cartoon\n");
   Alignment_Cartoon(ca, &al, 2, 4);
   FILE *aa = fopen("alignment.dat", "w");
   printf("trace\n");
-  Compute_Trace_MID(&al, work_data, 1);
+  Compute_Trace_MID(&al, work_data, 2);
   printf("print al\n");
   Print_Alignment(aa, &al, work_data, 2, 80, 5, 1, 1); 
 }
